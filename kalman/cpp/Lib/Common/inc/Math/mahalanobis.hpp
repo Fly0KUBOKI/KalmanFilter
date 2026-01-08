fc4bbe64af7f8a228c3541ee69dc7743461a22b2
Fly0KUBOKI <takuteru0225@gmail.com>
2026-01-08 07:36:56 +0000

    refactor(math): add unified mahalanobis wrapper and update MathUtils to use it

---
+++ kalman/cpp/Lib/Common/inc/Math/mahalanobis.hpp
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "../../../KF/inc/kf_operations.hpp"
+#include "../../../Matrix/fixed_matrix.hpp"
+
+namespace common {
+namespace math {
+
+using cm = cmath_fx::FixedMatrix;
+
+// Templated fixed-size variant -> forward to kf::mahalanobis_distance_squared (templated)
+template <int M, typename T>
+inline T mahalanobis_distance_squared(const cmath_fx::Vector<M, T>& innovation, const cmath_fx::Matrix<M, M, T>& S) {
+    return kf::mahalanobis_distance_squared<M, T>(innovation, S);
+}
+
+// Runtime-size FixedMatrix variant -> forward to kf::ops implementation
+inline float mahalanobis_distance_squared(const cm& innovation, const cm& S) {
+    return kf::ops::mahalanobis_distance_squared(innovation, S);
+}
+
+} // namespace math
+} // namespace common
